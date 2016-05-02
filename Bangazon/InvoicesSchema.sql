f80a6c9b0c556fe70d2dca7fea2853255a58b9da
Palmer Harrell <palmerharrell@me.com>
2016-05-02 09:51:15 -0500

    Created Invoices database

---
+++ Bangazon/InvoicesSchema.sql
@@ -0,0 +1,83 @@
+﻿CREATE TABLE Customer (
+    IdCustomer     INT           NOT NULL,
+    FirstName          VARCHAR (55)  NOT NULL,
+    LastName   VARCHAR (55) NOT NULL,
+    StreetAddress         VARCHAR(255)          NOT NULL,
+    City VARCHAR(55)           NOT NULL,
+    StateProvince VARCHAR (255) NOT NULL,
+    PostalCode VARCHAR(10) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdCustomer] ASC)
+);
+
+CREATE TABLE OrderProducts(
+  IdOrderProducts INT NOT NULL,
+  IdProduct INT NOT NULL,
+  IdOrder INT NOT NULL,
+  PRIMARY KEY CLUSTERED (IdOrderProducts ASC)
+);
+
+CREATE TABLE CustomerOrder (
+  IdOrder INT NOT NULL,
+  OrderNumber VARCHAR(20) NOT NULL,
+  DateCreated DATE NOT NULL,
+  IdCustomer INT NOT NULL,
+  IdPaymentOption INT NOT NULL,
+  ShippingMethod VARCHAR(15) NOT NULL,
+  PRIMARY KEY CLUSTERED (IdOrder ASC)
+);
+
+CREATE TABLE PaymentOption (
+  IdPaymentOption INT NOT NULL,
+  IdCustomer INT NOT NULL,
+  Name VARCHAR(99) NOT NULL,
+  AccountNumber VARCHAR(20) NOT NULL,
+  PRIMARY KEY CLUSTERED (IdPaymentOption ASC)
+);
+
+CREATE TABLE Product (
+  IdProduct INT NOT NULL,
+  IdProductType INT NOT NULL,
+  Name VARCHAR(99) NOT NULL,
+  Price REAL NOT NULL,
+  [Description] VARCHAR(255) NOT NULL,
+  Quantity INT NOT NULL,
+  PRIMARY KEY CLUSTERED (IdProduct ASC)
+);
+
+CREATE TABLE [dbo].[ProductType] (
+    [IdProductType] INT           NOT NULL,
+    [Name]          VARCHAR (55)  NOT NULL,
+    [Description]   VARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdProductType] ASC)
+);
+
+ALTER TABLE OrderProducts 
+  ADD CONSTRAINT [FK_OrderProducts_Order] 
+  FOREIGN KEY ([IdOrder]) 
+  REFERENCES [dbo].[CustomerOrder] ([IdOrder]);
+
+  
+ALTER TABLE OrderProducts 
+  ADD CONSTRAINT [FK_OrderProducts_Product] 
+  FOREIGN KEY ([IdProduct]) 
+  REFERENCES [dbo].[Product] ([IdProduct]);
+
+  
+ALTER TABLE CustomerOrder 
+  ADD CONSTRAINT [FK_CustomerOrder_Customer] 
+  FOREIGN KEY ([IdCustomer]) 
+  REFERENCES [dbo].[Customer] ([IdCustomer]);
+
+  
+ALTER TABLE CustomerOrder 
+  ADD CONSTRAINT [FK_CustomerOrder_PaymentOptions] 
+  FOREIGN KEY ([IdPaymentOption]) 
+  REFERENCES [dbo].[PaymentOption] ([IdPaymentOption]);
+
+  
+ALTER TABLE PaymentOption 
+  ADD CONSTRAINT [FK_PaymentOption_Customer] 
+  FOREIGN KEY ([IdCustomer]) 
+  REFERENCES [dbo].[Customer] ([IdCustomer]);
+
