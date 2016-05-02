2b0b8f5d04d97df6661a6f72415ec24a999a3a76
Palmer Harrell <palmerharrell@me.com>
2016-05-02 14:56:53 -0500

    Created main menu

--- Bangazon/Program.cs
+++ Bangazon/Program.cs
@@ -13,18 +13,31 @@ namespace Bangazon
     {
       InvoiceDb db = new InvoiceDb();
 
-      // ~~~ getProducts TEST ~~~
-      List<Product> allProducts = db.getProducts();
-      foreach(Product prod in allProducts)
-      {
-        Console.WriteLine("\nID: {0}", prod.idProduct);
-        Console.WriteLine("Type ID: {0}", prod.idProductType);
-        Console.WriteLine("Name: {0}", prod.name);
-        Console.WriteLine("Price: {0}", prod.price);
-        Console.WriteLine("Description: {0}", prod.description);
-      }
+      // Main Menu
+      Console.WriteLine("\n*********************************************************");
+      Console.WriteLine("**  Welcome to Bangazon! Command Line Ordering System  **");
+      Console.WriteLine("*********************************************************\n");
+      Console.WriteLine("1.Create an account");
+      Console.WriteLine("2.Create a payment option");
+      Console.WriteLine("3.Order a product");
+      Console.WriteLine("4.Complete an order");
+      Console.WriteLine("5.See product popularity");
+      Console.WriteLine("6.Leave Bangazon!");
+      Console.Write(">");
+      Console.Read();
 
-      
+      // // ~~~ getProducts TEST ~~~
+      // List<Product> allProducts = db.getProducts();
+      // foreach(Product prod in allProducts)
+      // {
+      //   Console.WriteLine("\nID: {0}", prod.idProduct);
+      //   Console.WriteLine("Type ID: {0}", prod.idProductType);
+      //   Console.WriteLine("Name: {0}", prod.name);
+      //   Console.WriteLine("Price: {0}", prod.price);
+      //   Console.WriteLine("Description: {0}", prod.description);
+      // }
+
+
     } // Main
   } // Program Class
 } // Namespace
