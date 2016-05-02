58102491a7c39d26d4539bb8ee14f900f021f237
Palmer Harrell <palmerharrell@me.com>
2016-05-02 14:42:03 -0500

    Created PaymentOption class

---
+++ Bangazon/PaymentOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangazon
+{
+  public class PaymentOption
+  {
+    public int idPaymentOption { get; set; }
+    public int idCustomer { get; set; }
+    public string name { get; set; }
+    public int accountNumber { get; set; }
+  }
+}
+
